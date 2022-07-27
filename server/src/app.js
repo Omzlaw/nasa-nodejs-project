fcfc87fcefd78c067948449d53ab47596fa81bee
Omzlaw <omeizaalabi@gmail.com>
2022-07-28 00:05:54 +0100

    Add new npm scripts to run npm scripts in server and client Add script to build and copy react app build files to public folder in server directory

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,19 @@
-
+const path = require('path');
 const express = require('express');
+const cors = require('cors');
 
 const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
+
+// Middlewares
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+//Routers
 app.use('/planets', planetsRouter);
 
 module.exports = app;
--- server/src/server.js
+++ server/src/server.js
@@ -3,8 +3,21 @@ const http = require('http');
 const app = require('./app');
 const server = http.createServer(app);
 
+const {
+    loadPlanetsData
+} = require('./models/planets.model');
 const PORT = process.env.PORT || 8000;
-server.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
 
+async function loadInitialData() {
+    await loadPlanetsData();
+}
+
+
+async function startServer() {
+    await loadInitialData();
+    server.listen(PORT, () => {
+        console.log('Server is listening on port: ' + PORT);
+    });
+}
+
+startServer();
