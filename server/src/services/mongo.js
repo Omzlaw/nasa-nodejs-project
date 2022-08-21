e100a199f6b79922cab23fc12a29f3d234f62459
Omzlaw <omeizaalabi@gmail.com>
2022-08-22 00:15:43 +0100

    Add and use dotenv package

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL = 'mongodb+srv://nasa-api:XpmIV8guVFUw4ZSu@nasa-api-cluster.rg7z9.mongodb.net/?retryWrites=true&w=majority';
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
     console.log('MongoDB connection ready!');
