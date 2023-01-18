1cd63d82b4273f88712b7165ac85bcb34a672769
Omzlaw <omeizaalabi@gmail.com>
2023-01-18 23:45:53 +0100

    Adding mutations

--- graphql-example/products/products.model.js
+++ graphql-example/products/products.model.js
@@ -2,12 +2,14 @@ const products = [
     {
         id: 'redshoe',
         description: 'Red Shoe',
-        price: 42.13
+        price: 42.13,
+        reviews: [],
     },
     {
         id: 'bluejean',
         description: 'Blue Jeans',
-        price: 42.13
+        price: 42.13,
+        reviews: [],
     },
 ]
 
@@ -17,7 +19,7 @@ function getAllProducts() {
 
 function getProductsByPrice(minPrice, maxPrice) {
     return products.filter((product) => {
-        return product.price >= minPrice && product.price <= max;
+        return product.price >= minPrice && product.price <= maxPrice;
     })
 }
 
@@ -27,8 +29,37 @@ function getProductById(id) {
     })
 }
 
+function addNewProduct(id, description, price) {
+    const newProduct = {
+        id,
+        price,
+        description,
+        reviews: []
+    }
+
+    products.push(newProduct);
+    return newProduct;
+}
+
+function addNewProductReview(id, rating, comment) {
+    const matchedProduct = getProductById(id);
+    if (matchedProduct) {
+        const newProductReview = {
+            rating,
+            comment
+        }
+
+        matchedProduct.reviews.push(newProductReview)
+
+        return newProductReview;
+    }
+    return null;
+}
+
 module.exports = {
     getAllProducts,
     getProductsByPrice,
-    getProductById
+    getProductById,
+    addNewProduct,
+    addNewProductReview
 }
